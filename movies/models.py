14bd5dd2ce94738d343b8c8fef4c93d56b3ab5cf
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-06-26 18:36:17 +0200

    update 2

--- movies/models.py
+++ movies/models.py
@@ -5,10 +5,13 @@ from django.utils import timezone
 class Genre(models.Model):
     name=models.CharField(max_length=255) #text based db column
 
+    def __str__(self):
+        return self.name
+
 class Movie(models.Model):
     title=models.CharField(max_length=255)
-    release_year=models.IntegerField()
-    number_in_stock=models.IntegerField()
-    daily_rate=models.FloatField()
-    genre=models.ForeignKey(Genre, on_delete=models.CASCADE)
+    release_year=models.IntegerField(default=None)
+    number_in_stock=models.IntegerField(default=None)
+    daily_rate=models.FloatField(null=True, blank=True, default=None)
+    genre=models.ForeignKey(Genre, on_delete=models.CASCADE, default='')
     date_created=models.DateTimeField(default=timezone.now)
