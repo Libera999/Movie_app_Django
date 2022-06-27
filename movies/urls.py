878a8531ca35b5e26e22460febdab5c0ebac330e
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-06-27 22:03:27 +0200

    up 3

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views # import views from current directory
 
 urlpatterns = [
-   path('',views.index, name = 'index') #home page brings us to index and it will have a name of index
+   path('',views.index, name = 'movies_index'), #home page brings us to index and it will have a name of index
+   path('<movie_id>', views.detail,name='movies_detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render #render template
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi, it's Mariam")
+    movies=Movie.objects.all() #SELECT * FROM movies_movie - gets a list of movies from the DB
+    output=render(request,'movies/index.html',{'movies': movies}) 
+    
+    #Movie.objects.filter(release_year=1999) #SELECT * FROM movies_movie WHERE 
+    #Movie.objects.get(id=1) #SELECT * FROM movies_movie
+    
+    return HttpResponse(output)
+
+def detail(request, movie_id):
+    movie=Movie.objects.get(pk=movie_id)
+
+    return render(request, 'movies/detail.html',{'movies': movie})
