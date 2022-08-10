e1ffdb4549a10569e466cf31a11cbce2bc03ad9a
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-08-10 18:42:36 +0300

    Update settings.py
    Hide the SK

--- vidly/settings.py
+++ vidly/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nx=$tmfh!@-=)7^u!wj%s$mq60i3oem!z%l#@_@ou4kxyh^40k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
